be39a699a4706a74ae167254915ab4d5e0c530dd
Georgi Georgie <Georgi Georgie@DESKTOP-9FI4TKH>
2025-02-27 15:39:10 +0200

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] matrix = new int[4, 4];
+int number = 1;
+
+for (int row = 0; row < 4; row++)
+{
+    for (int col = 0; col < 4; col++)
+    {
+        matrix[row, col] = number;
+        number++;
+    }
+}
+
+Console.WriteLine("Matrix 4x4:");
+for (int row = 0; row < 4; row++)
+{
+    for (int col = 0; col < 4; col++)
+    {
+        Console.Write($"{matrix[row, col],3} ");
+    }
+    Console.WriteLine();
+}
